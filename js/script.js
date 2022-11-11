0165a761aa7c493f478bee03c6a2ec451fff4de9
LeoNumbela <leonumbela@gmail.com>
2022-11-11 10:05:05 -0300

    se inicia releases 3 con diseno multilenguage

--- js/script.js
+++ js/script.js
@@ -62,9 +62,24 @@ $(document).ready(function(){
 
 });
 
- 
+const flagsElement = document.getElementById("Flags");
 
+const textsToChange = document.querySelectorAll("[data-section]");
 
+const changeLanguage = async language =>{
+    const requestJson = await fetch(`./languages/${language}.json`)
+    const texts = await requestJson.json()
+
+    for (const textToChange of textsToChange) {
+        const section = textToChange.dataset.section;
+        const value = textToChange.dataset.value;
+        textToChange.innerHTML = texts[section][value];
+    }
+}
+
+flagsElement.addEventListener('click', (e) =>{
+    changeLanguage(e.target.parentElement.dataset.language)
+});
 
 
 
